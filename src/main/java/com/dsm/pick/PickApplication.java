ce09ae65dc313c9f24c9326e426b8a1cad60ee86
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-08-09 17:21:14 +0900

    [CREATE] Started Project

---
+++ src/main/java/com/dsm/pick/PickApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.pick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickApplication.class, args);
+	}
+
+}
