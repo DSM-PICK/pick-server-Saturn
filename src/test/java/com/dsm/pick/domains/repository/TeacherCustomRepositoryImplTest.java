a68c8b02e13825fadeb99a1b12a202d888e664c2
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-09-29 15:09:28 +0900

    [UPDATE] @DataJpaTest -> @SpringBootTest

--- src/test/java/com/dsm/pick/domains/repository/TeacherCustomRepositoryImplTest.java
+++ src/test/java/com/dsm/pick/domains/repository/TeacherCustomRepositoryImplTest.java
@@ -9,20 +9,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
-@TestPropertySource(properties = {
-        "spring.jpa.database-platform=org.hibernate.dialect.MySQL5InnoDBDialect",
-        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
-        "spring.datasource.url=jdbc:mysql://localhost:3306/testdb_saturn?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-        "spring.datasource.username=root",
-        "spring.datasource.password=1111"
-})
+//@DataJpaTest
+@SpringBootTest
+//@TestPropertySource(properties = {
+//        "spring.jpa.database-platform=org.hibernate.dialect.MySQL5InnoDBDialect",
+//        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
+//        "spring.datasource.url=jdbc:mysql://localhost:3306/testdb_saturn?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+//        "spring.datasource.username=root",
+//        "spring.datasource.password=1111"
+//})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TeacherCustomRepositoryImplTest {
 
