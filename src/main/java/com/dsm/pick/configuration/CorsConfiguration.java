157e9e97ea50e2801e9eabedfceb12271d680ff5
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-12 12:41:25 +0900

    [UPDATE] CorsConfiguration

--- src/main/java/com/dsm/pick/configuration/CorsConfiguration.java
+++ src/main/java/com/dsm/pick/configuration/CorsConfiguration.java
@@ -6,19 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/saturn/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowCredentials(false)
-                        .maxAge(3000);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
     }
 }
