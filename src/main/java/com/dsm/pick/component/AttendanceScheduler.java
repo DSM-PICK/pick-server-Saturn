4b55350445682c808b4a5df2f06b360ba69cf4c1
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-15 00:10:17 +0900

    알게뭐야

--- src/main/java/com/dsm/pick/component/AttendanceScheduler.java
+++ src/main/java/com/dsm/pick/component/AttendanceScheduler.java
@@ -42,7 +42,7 @@ public class AttendanceScheduler {
 
     //    @Scheduled(cron = "0/1 * * * * *")        => TEST
 //    @Scheduled(cron = "1 0 0/1 * * MON-FRI")      => REAL
-    @Scheduled(cron = "1 0 0 * * MON-FRI")
+    @Scheduled(cron = "1 20 0 * * MON-FRI")
     public void createTodayAttendance() {
         final LocalDate date = LocalDate.now();
         logger.info("date : " + date.toString());
