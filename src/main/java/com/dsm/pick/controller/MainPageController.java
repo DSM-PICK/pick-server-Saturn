b255a4f49facb11788f82bbdec43745f22669a59
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-18 20:24:47 +0900

    [ADD] dummy data

---
+++ src/main/java/com/dsm/pick/controller/MainPageController.java
@@ -0,0 +1,12 @@
+package com.dsm.pick.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+@Api(value = "Main Page Controller")
+public class MainPageController {
+
+}
---
+++ src/main/java/com/dsm/pick/domains/service/NoticeService.java
@@ -0,0 +1,8 @@
+package com.dsm.pick.domains.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+
+}
