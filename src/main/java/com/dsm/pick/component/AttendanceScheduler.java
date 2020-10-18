3e8b59eeaae1be129ef598dac938d636e7f63018
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-10-18 20:30:08 +0900

    [UPDATE] build.gradle

--- src/main/java/com/dsm/pick/component/AttendanceScheduler.java
+++ src/main/java/com/dsm/pick/component/AttendanceScheduler.java
@@ -40,7 +40,7 @@ public class AttendanceScheduler {
 
 //    @Scheduled(cron = "0/1 * * * * *")              // TEST
 //    @Scheduled(cron = "1 0 0 * * MON-FRI")          // REAL
-    @Scheduled(cron = "1 34 20 * * *")        // TEST
+    @Scheduled(cron = "1 40 20 * * *")        // TEST
     public void createTodayAttendance() {
         final LocalDate date = LocalDate.now();
         final Activity activity = activityRepository.findById(date)
