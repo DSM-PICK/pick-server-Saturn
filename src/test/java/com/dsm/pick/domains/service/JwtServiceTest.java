6e2d88487adc18ec57fc93e0df3f7f9938e2b9c5
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-20 12:39:16 +0900

    dfasdf

--- src/test/java/com/dsm/pick/domains/service/JwtServiceTest.java
+++ src/test/java/com/dsm/pick/domains/service/JwtServiceTest.java
@@ -1,59 +1,59 @@
-package com.dsm.pick.domains.service;
-
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class JwtServiceTest {
-
-    JwtService jwtService = new JwtService("dhwlddjgmanf");
-
-    @Test
-    void createAccessToken() {
-        String teacherId = "kiwi";
-        String accessToken = jwtService.createAccessToken(teacherId);
-    }
-
-    @Test
-    void createRefreshToken() {
-        String teacherId = "lemon";
-        String refreshToken = jwtService.createRefreshToken(teacherId);
-    }
-
-    @Test
-    void getTeacherId() {
-        String teacherId = "richie";
-        String accessToken = jwtService.createAccessToken(teacherId);
-
-        String findTeacherId = jwtService.getTeacherId(accessToken);
-
-        assertEquals(teacherId, findTeacherId);
-    }
-
-    @Test
-    void getExpiration() {
-        String teacherId = "quince";
-        String accessToken = jwtService.createAccessToken(teacherId);
-        Date expiration = jwtService.getExpiration(accessToken);
-    }
-
-    @Test
-    void isUsableToken() {
-        String teacherId = "jujube";
-        String accessToken = jwtService.createAccessToken(teacherId);
-
-        boolean isValid = jwtService.isValid(accessToken);
-        boolean isTimeOut = jwtService.isNotTimeOut(accessToken);
-
-        assertTrue(isValid && isTimeOut);
-    }
-}
+//package com.dsm.pick.domains.service;
+//
+//import io.jsonwebtoken.Jwt;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//
+//import javax.crypto.spec.SecretKeySpec;
+//import javax.xml.bind.DatatypeConverter;
+//import java.security.Key;
+//import java.util.Date;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class JwtServiceTest {
+//
+//    JwtService jwtService = new JwtService("dhwlddjgmanf");
+//
+//    @Test
+//    void createAccessToken() {
+//        String teacherId = "kiwi";
+//        String accessToken = jwtService.createAccessToken(teacherId);
+//    }
+//
+//    @Test
+//    void createRefreshToken() {
+//        String teacherId = "lemon";
+//        String refreshToken = jwtService.createRefreshToken(teacherId);
+//    }
+//
+//    @Test
+//    void getTeacherId() {
+//        String teacherId = "richie";
+//        String accessToken = jwtService.createAccessToken(teacherId);
+//
+//        String findTeacherId = jwtService.getTeacherId(accessToken);
+//
+//        assertEquals(teacherId, findTeacherId);
+//    }
+//
+//    @Test
+//    void getExpiration() {
+//        String teacherId = "quince";
+//        String accessToken = jwtService.createAccessToken(teacherId);
+//        Date expiration = jwtService.getExpiration(accessToken);
+//    }
+//
+//    @Test
+//    void isUsableToken() {
+//        String teacherId = "jujube";
+//        String accessToken = jwtService.createAccessToken(teacherId);
+//
+//        boolean isValid = jwtService.isValid(accessToken);
+//        boolean isTimeOut = jwtService.isNotTimeOut(accessToken);
+//
+//        assertTrue(isValid && isTimeOut);
+//    }
+//}
