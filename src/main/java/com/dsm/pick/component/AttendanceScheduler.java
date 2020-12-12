b88fb210d397b515e3aa902a23318c589540d180
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-12 23:15:41 +0900

    <AttendanceScheduler> 스케줄링

--- src/main/java/com/dsm/pick/component/AttendanceScheduler.java
+++ src/main/java/com/dsm/pick/component/AttendanceScheduler.java
@@ -36,7 +36,7 @@ public class AttendanceScheduler {
 
 //    @Scheduled(cron = "0/1 * * * * *")              // TEST
 //    @Scheduled(cron = "1 0 0 * * MON-FRI")          // REAL
-    @Scheduled(cron = "1 10 23 * * *")        // TEST//
+    @Scheduled(cron = "1 20 23 * * *")        // TEST//
     public void createTodayAttendance() {
         final LocalDate date = LocalDate.now();
         final Activity activity = activityRepository.findById(date)
