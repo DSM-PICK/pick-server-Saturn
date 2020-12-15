29b131fd09d525027596cebcff21e2286ee07687
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-15 11:01:10 +0900

    <AuthService> Remove Comment

--- src/main/java/com/dsm/pick/domains/service/AuthService.java
+++ src/main/java/com/dsm/pick/domains/service/AuthService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.util.Optional;
 
 @Service
 public class AuthService {
