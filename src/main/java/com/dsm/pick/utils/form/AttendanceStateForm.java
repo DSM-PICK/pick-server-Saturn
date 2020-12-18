6043f1eb73a1b23e8347447dfc200ca9fe25a236
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-18 10:35:36 +0900

    <AttendanceContrller> AttendanceListForm에 7교시 메모, 8교시 메모... 등등 생성

--- src/main/java/com/dsm/pick/utils/form/AttendanceStateForm.java
+++ src/main/java/com/dsm/pick/utils/form/AttendanceStateForm.java
@@ -7,21 +7,37 @@ public class AttendanceStateForm {
     @ApiModelProperty(example = "출석")
     private String seven;
 
+    @ApiModelProperty(example = "메모메모")
+    private String sevenMemo;
+
     @ApiModelProperty(example = "출석", required = true)
     private String eight;
+
+    @ApiModelProperty(example = "메모메모")
+    private String eightMemo;
 
     @ApiModelProperty(example = "출석", required = true)
     private String nine;
 
+    @ApiModelProperty(example = "메모메모")
+    private String nineMemo;
+
     @ApiModelProperty(example = "출석", required = true)
     private String ten;
 
+    @ApiModelProperty(example = "메모메모")
+    private String tenMemo;
+
     public AttendanceStateForm() {}
-    public AttendanceStateForm(String seven, String eight, String nine, String ten) {
+    public AttendanceStateForm(String seven, String sevenMemo, String eight, String eightMemo, String nine, String nineMemo, String ten, String tenMemo) {
         this.seven = seven;
+        this.sevenMemo = sevenMemo;
         this.eight = eight;
+        this.eightMemo = eightMemo;
         this.nine = nine;
+        this.nineMemo = nineMemo;
         this.ten = ten;
+        this.tenMemo = tenMemo;
     }
 
     public String getSeven() {
@@ -32,12 +48,28 @@ public class AttendanceStateForm {
         this.seven = seven;
     }
 
+    public String getSevenMemo() {
+        return sevenMemo;
+    }
+
+    public void setSevenMemo(String sevenMemo) {
+        this.sevenMemo = sevenMemo;
+    }
+
     public String getEight() {
         return eight;
     }
 
     public void setEight(String eight) {
         this.eight = eight;
+    }
+
+    public String getEightMemo() {
+        return eightMemo;
+    }
+
+    public void setEightMemo(String eightMemo) {
+        this.eightMemo = eightMemo;
     }
 
     public String getNine() {
@@ -48,11 +80,27 @@ public class AttendanceStateForm {
         this.nine = nine;
     }
 
+    public String getNineMemo() {
+        return nineMemo;
+    }
+
+    public void setNineMemo(String nineMemo) {
+        this.nineMemo = nineMemo;
+    }
+
     public String getTen() {
         return ten;
     }
 
     public void setTen(String ten) {
         this.ten = ten;
+    }
+
+    public String getTenMemo() {
+        return tenMemo;
+    }
+
+    public void setTenMemo(String tenMemo) {
+        this.tenMemo = tenMemo;
     }
 }
