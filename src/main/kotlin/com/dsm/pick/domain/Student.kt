b419b7421e04157b19c5a575c37b3a5030069c7c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-02-07 20:43:11 +0900

    코틀린 이전

--- src/main/kotlin/com/dsm/pick/domain/Student.kt
+++ src/main/kotlin/com/dsm/pick/domain/Student.kt
@@ -26,4 +26,31 @@ class Student(
 
     @Column(name = "is_tuesday_self_study")
     val isTuesdaySelfStudy: Boolean,
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Student
+
+        if (number != other.number) return false
+        if (name != other.name) return false
+        if (club != other.club) return false
+        if (classroom != other.classroom) return false
+        if (isMondaySelfStudy != other.isMondaySelfStudy) return false
+        if (isTuesdaySelfStudy != other.isTuesdaySelfStudy) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = number.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + club.hashCode()
+        result = 31 * result + classroom.hashCode()
+        result = 31 * result + isMondaySelfStudy.hashCode()
+        result = 31 * result + isTuesdaySelfStudy.hashCode()
+        return result
+    }
+}
