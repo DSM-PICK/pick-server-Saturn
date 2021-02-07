901becd54a14ab5066253606b50340cfc5f84858
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-02-07 19:15:17 +0900

    코틀린 이전

--- src/main/kotlin/com/dsm/pick/controller/NoticeController.kt
+++ src/main/kotlin/com/dsm/pick/controller/NoticeController.kt
@@ -3,7 +3,9 @@ package com.dsm.pick.controller
 import com.dsm.pick.service.NoticeService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("/main")
 class NoticeController(
     private val noticeService: NoticeService,
