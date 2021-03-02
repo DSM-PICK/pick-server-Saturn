6cccb357ced60e276da0cc7f43569c5e43c23938
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-02 19:04:22 +0900

    daf

--- src/main/kotlin/com/dsm/pick/controller/request/StudentStateRequest.kt
+++ src/main/kotlin/com/dsm/pick/controller/request/StudentStateRequest.kt
@@ -5,7 +5,7 @@ import com.dsm.pick.domain.attribute.State
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
-data class StudentStateRequest(
+class StudentStateRequest(
     @NotNull(message = "허용하지 않는 형식 <NULL>")
     val period: Period,
     @NotBlank(message = "허용하지 않는 형식 <NULL, EMPTY, BLANK>")
