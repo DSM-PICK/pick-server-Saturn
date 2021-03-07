361f534a9cce64473c4a11922f87953b8885842e
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-08 08:39:08 +0900

    The API for importing attendance status changed the date of attendance status to be selected.

--- src/main/kotlin/com/dsm/pick/controller/AttendanceController.kt
+++ src/main/kotlin/com/dsm/pick/controller/AttendanceController.kt
@@ -40,8 +40,7 @@ class AttendanceController(
         @PathVariable("schedule") schedule: Schedule,
         @PathVariable("floor") floor: Floor,
         @PathVariable("priority") priority: Int,
-        @RequestParam("date", required = false)
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate = LocalDate.now()
+        @RequestParam("date", defaultValue = "#{T(java.time.LocalDateTime).now()}") date: LocalDate,
     ): AttendanceResponse {
         authService.validateToken(token)
         return attendanceService.showAttendance(schedule, floor, priority, date)
