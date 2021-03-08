39b70a6337cbfcd43b3959b135929d30c423a46a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-08 11:02:46 +0900

    Changed the validation level of the StudentStateRequest class

--- src/main/kotlin/com/dsm/pick/controller/request/StudentStateRequest.kt
+++ src/main/kotlin/com/dsm/pick/controller/request/StudentStateRequest.kt
@@ -1,13 +1,17 @@
 package com.dsm.pick.controller.request
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 class StudentStateRequest(
     @NotNull(message = "허용하지 않는 형식 <NULL>")
+    @Min(value = 1, message = "1 미만 숫자는 허용하지 않습니다.")
+    @Max(value = 10, message = "10 초과 숫자는 허용하지 않습니다.")
     val period: Int,
     @NotBlank(message = "허용하지 않는 형식 <NULL, EMPTY, BLANK>")
     val number: String,
-    @NotNull(message = "허용하지 않는 형식 <NULL>")
+    @NotBlank(message = "허용하지 않는 형식 <NULL, EMPTY, BLANK>")
     val state: String,
 )
