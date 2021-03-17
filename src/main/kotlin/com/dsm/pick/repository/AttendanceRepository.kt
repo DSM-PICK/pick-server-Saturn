0753f2c60f9856bb739bc6acabb7d10617551918
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-17 12:56:54 +0900

    Add @Modifying annotation in AttendanceRepository.

--- src/main/kotlin/com/dsm/pick/repository/AttendanceRepository.kt
+++ src/main/kotlin/com/dsm/pick/repository/AttendanceRepository.kt
@@ -5,6 +5,7 @@ import com.dsm.pick.domain.attribute.Floor
 import com.dsm.pick.domain.attribute.Period
 import com.dsm.pick.domain.attribute.State
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import java.time.LocalDate
@@ -15,6 +16,7 @@ interface AttendanceRepository : JpaRepository<Attendance, Int> {
     fun findByStudentNumberAndPeriodAndActivityDate(studentNumber: String, period: Period, attendanceDate: LocalDate): Attendance?
     fun findByActivityDateAndStudentNumberStartingWith(date: LocalDate, grade: String): List<Attendance>
 
+    @Modifying
     @Query(value = "UPDATE Attendance a SET a.state = :state WHERE a.student.number IN :numbers AND a.period IN :periods AND a.activity.date = :date")
     fun test(
         @Param("state") state: State,
