8d8a50d2a29355276c75292e696d502871f67fa8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-15 16:08:51 +0900

    solved a jpa n + 1.

--- src/main/kotlin/com/dsm/pick/repository/ClubRepository.kt
+++ src/main/kotlin/com/dsm/pick/repository/ClubRepository.kt
@@ -2,9 +2,11 @@ package com.dsm.pick.repository
 
 import com.dsm.pick.domain.Club
 import com.dsm.pick.domain.attribute.Floor
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ClubRepository : JpaRepository<Club, String> {
+    @EntityGraph(attributePaths = ["students", "location"])
     fun findByLocationFloor(floor: Floor): List<Club>
     fun findByLocationFloorAndLocationPriority(floor: Floor, priority: Int): Club?
     fun findByTeacher(teacherName: String): List<Club>
